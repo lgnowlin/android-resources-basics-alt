e9671b8c49d539dcfb943b6eeed9b19ea807f536
lgnowlin <lgnowlin@ualr.edu>
2020-10-08 21:10:31 -0500

    Questions 3-8

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.content.res.Resources;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -13,11 +15,19 @@ import com.google.android.material.snackbar.Snackbar;
 // TODO 5: Initialize the text property of the TextView element with the "country_description_text" id by using the value retrieved in the previous TODO point
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView country;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.country = findViewById(R.id.country_description_text);
+        Resources res = getResources();
+        String country_text = res.getString(R.string.country_description);
+        country.setText(country_text);
+
     }
+
+
 
 }
